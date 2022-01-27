7f7b4ee606dc23e2befe73ea3a802dbf1870494b
Wonyoung Jang <43818471+Stomper10@users.noreply.github.com>
2022-01-27 23:25:31 +0900

    Update pages

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Hello",
-        "excerpt":"This is my new blog. Welcome.  ","categories": [],
+        "excerpt":"This is my new blog. Welcome.  ","categories": ["blog"],
         "tags": ["blog"],
         "url": "/2022-01-17-01/",
         "teaser": null
