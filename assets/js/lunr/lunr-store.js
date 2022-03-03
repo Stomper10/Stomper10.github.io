6568d8d8b78ddc4d0759e22d91065d7dd1bfd4e2
Wonyoung Jang <43818471+Stomper10@users.noreply.github.com>
2022-03-03 18:38:44 +0900

    Site updated at 2022-03-03 09:38:44 UTC

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "안녕하세요",
+        "excerpt":"새 블로그입니다. 반갑습니다.  ","categories": ["blog"],
+        "tags": ["blog"],
+        "url": "/2022-02-10-01/",
+        "teaser": null
+      },{
+        "title": "새 학기",
+        "excerpt":"새 학기가 시작됐습니다.  ","categories": ["blog"],
+        "tags": ["blog"],
+        "url": "/2022-03-03-01/",
+        "teaser": null
+      }]
---
+++ en/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Hello",
+        "excerpt":"This is my new blog. Welcome.  ","categories": ["blog"],
+        "tags": ["blog"],
+        "url": "/2022-02-10-01/",
+        "teaser": null
+      },{
+        "title": "New Semester",
+        "excerpt":"Starting the new semester.  ","categories": ["blog"],
+        "tags": ["blog"],
+        "url": "/2022-03-03-01/",
+        "teaser": null
+      }]
